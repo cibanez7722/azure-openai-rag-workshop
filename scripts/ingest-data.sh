67e6c86a5466cdcea6214087cafb8029cc668243
cibanez7722 <claudio.ibanez@arauco.com>
2024-10-21 22:17:04 +0000

    Test5

--- scripts/ingest-data.sh
+++ scripts/ingest-data.sh
@@ -11,3 +11,7 @@ curl -F "file=@./data/privacy-policy.pdf" "${INGESTION_API_URI:-http://localhost
 curl -F "file=@./data/support.pdf" "${INGESTION_API_URI:-http://localhost:3001}/documents"
 curl -F "file=@./data/terms-of-service.pdf" "${INGESTION_API_URI:-http://localhost:3001}/documents"
 curl -F "file=@./data/2024.03-Corporate-Presentation-VF.pdf" "${INGESTION_API_URI:-http://localhost:3001}/documents"
+curl -F "file=@./data/20-F 2023.pdf" "${INGESTION_API_URI:-http://localhost:3001}/documents"
+curl -F "file=@./data/2024.03-Press-Release-web-2.pdf" "${INGESTION_API_URI:-http://localhost:3001}/documents"
+curl -F "file=@./data/Conference-Call-1Q-2024-VF-web.pdf" "${INGESTION_API_URI:-http://localhost:3001}/documents"
+curl -F "file=@./data/Estados_financieros_PDF93458000_202403.pdf" "${INGESTION_API_URI:-http://localhost:3001}/documents"
