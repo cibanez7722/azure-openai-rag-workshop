a665eed6dbaaf0c422ec950c61b41471d5f7dd29
cibanez7722 <claudio.ibanez@arauco.com>
2024-10-22 13:24:07 +0000

    Test13

--- scripts/ingest-data.sh
+++ scripts/ingest-data.sh
@@ -7,11 +7,6 @@ if azd_env=$(azd env get-values); then
 fi
 
 echo 'Uploading PDF files to the ingestion API'
-curl -F "file=@./data/privacy-policy.pdf" "${INGESTION_API_URI:-http://localhost:3001}/documents"
-curl -F "file=@./data/support.pdf" "${INGESTION_API_URI:-http://localhost:3001}/documents"
-curl -F "file=@./data/terms-of-service.pdf" "${INGESTION_API_URI:-http://localhost:3001}/documents"
-curl -F "file=@./data/2024.03-Corporate-Presentation-VF.pdf" "${INGESTION_API_URI:-http://localhost:3001}/documents"
-curl -F "file=@./data/20-F 2023.pdf" "${INGESTION_API_URI:-http://localhost:3001}/documents"
-curl -F "file=@./data/2024.03-Press-Release-web-2.pdf" "${INGESTION_API_URI:-http://localhost:3001}/documents"
-curl -F "file=@./data/Conference-Call-1Q-2024-VF-web.pdf" "${INGESTION_API_URI:-http://localhost:3001}/documents"
-curl -F "file=@./data/Estados_financieros_PDF93458000_202403.pdf" "${INGESTION_API_URI:-http://localhost:3001}/documents"
+
+curl -F "file=@./data/20_F_2022" "${INGESTION_API_URI:-http://localhost:3001}/documents"
+curl -F "file=@./data/Estados_financieros_2024.pdf.pdf" "${INGESTION_API_URI:-http://localhost:3001}/documents"
